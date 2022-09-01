fccc38c14575255b0ea02393345a2c2864156b27
RocketPhish <112438097+RocketPhish@users.noreply.github.com>
2022-09-01 16:49:00 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -109,23 +109,23 @@ export default {
           "mech": "Bonus Points"
         },
         {
-          "callsign": "Kal",
-          "alias": "Vier Flau",
+          "callsign": "Nautilus",
+          "alias": "Sandy Britschgi",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Necrophile"
+          "corpro": "HA",
+          "frame": "Sunzi",
+          "mech": "Finale"
         },
         {
-          "callsign": "Starboard",
-          "alias": "Maria",
+          "callsign": "Big Bull",
+          "alias": "Keanu Rivera",
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Scarlet Judicator"
+          "corpro": "HA",
+          "frame": "Barbarossa",
+          "mech": "Everything Goes Black"
         },
         {
-          "callsign": "Badlands",
+          "callsign": "Empty",
           "alias": "Marlene Gatsby",
           "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
           "corpro": "GMS",
@@ -133,7 +133,7 @@ export default {
           "mech": "Mayfly"
         },
         {
-          "callsign": "Compadre",
+          "callsign": "Empty",
           "alias": 'Clint "CC" Cruz',
           "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
           "corpro": "GMS",
@@ -142,7 +142,7 @@ export default {
         },
       ],
       "header": {
-        "planet": "Hercynia",
+        "planet": "HERCYNIA",
         "year": "5014u",
         "system": "Ardennes-3",
         "gate": "Atlas-Quanokrim",
