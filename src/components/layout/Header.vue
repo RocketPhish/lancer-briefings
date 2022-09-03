46d96478b6ae8586eb37593bd644c2f46d6c7e81
RocketPhish <112438097+RocketPhish@users.noreply.github.com>
2022-09-03 18:21:57 -0400

    Update Header.vue

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -15,7 +15,7 @@
     </div>
     <div class="rhombus">&nbsp;</div>
     <video autoplay muted loop width="90px" height="90px">
-      <source src="Planet" type="jpg" />
+      <source src="public_planet" type="webm" />
     </video>
     <div class="location-info">
       <div class="location-row" id="planet-year">
