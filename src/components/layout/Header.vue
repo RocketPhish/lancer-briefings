cee64b7fe179e1b4498035c6a6b67b4acf7f7873
RocketPhish <112438097+RocketPhish@users.noreply.github.com>
2022-09-03 18:16:12 -0400

    Update Header.vue

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -15,7 +15,7 @@
     </div>
     <div class="rhombus">&nbsp;</div>
     <video autoplay muted loop width="90px" height="90px">
-      <source src="Atania-2" type="jpg" />
+      <source src="Planet" type="jpg" />
     </video>
     <div class="location-info">
       <div class="location-row" id="planet-year">
