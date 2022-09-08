20dd40dcee515704a5926e06adb42f99bb076875
RocketPhish <112438097+RocketPhish@users.noreply.github.com>
2022-09-07 21:46:05 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -105,8 +105,8 @@ export default {
           "alias": "Buddy Quasar",
           "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
           "corpro": "SSC",
-          "frame": "MetalMark",
-          "mech": "Bonus Points"
+          "frame": "Emperor",
+          "mech": "Worst of all possible worlds"
         },
         {
           "callsign": "Nautilus",
