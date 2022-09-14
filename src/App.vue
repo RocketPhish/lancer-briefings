746bd62ad5e8cbf088601873789853b08f212f13
RocketPhish <112438097+RocketPhish@users.noreply.github.com>
2022-09-13 20:08:54 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -112,9 +112,9 @@ export default {
           "callsign": "Nautilus",
           "alias": "Sandy Britschgi",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
-          "corpro": "HA",
-          "frame": "Sunzi",
-          "mech": "Finale"
+          "corpro": "Horus",
+          "frame": "Balor",
+          "mech": "Bulldozer Slate"
         },
         {
           "callsign": "Big Bull",
